c21e28d43b19a165b42184772d5f7dd6ff4f2f51
zumahun <80197742+zumahun@users.noreply.github.com>
2022-08-03 11:50:05 +0900

    Add files via upload

---
+++ srcs/sw_debounce.v
@@ -0,0 +1,16 @@
+module sw_debounce (
+    input clk,
+    input btn_in,   // using a button, not swtich , works the same
+    output btn_out
+);
+
+    reg t0, t1, t2;
+
+    always @(posedge clk) begin
+        t0 <= btn_in;
+        t1 <= t0;
+        t2 <= t1;   
+    end
+        
+    assign btn_out = t2;
+endmodule
